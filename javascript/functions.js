ac77dc2106ca1712c2f72d7ac7896de9ac07ee52
gracieux412 <gracieuxkitsa20@gmail.com>
2026-05-30 11:18:42 +0300

    adding functions file

---
+++ javascript/functions.js
@@ -0,0 +1,15 @@
+// simple functions
+function myFunction(){
+    console.log("Hello World")
+}
+myFunction()
+
+function myFunctionWithParams(name){
+    return "Hello "+name
+}
+console.log(myFunctionWithParams("Alice"))
+
+const printName = (name)=> {
+    return "Hello "+name
+}
+console.log(printName("Bob"))
